f335199fc32c683e787ca38768f42a5be1087d65
Derrick Walton <derrick.e.walton@gmail.com>
2019-07-08 22:28:39 -0600

    add new logic

---
+++ level1/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+def loadFile(filename):
+    myResult = __import__(filename)
+    if myResult.main() == 'Hello world':
+        return False
+    else:
+        return True
+
+
+def args():
+    '''This function just loads the argmunts by default the assumption
+    is made that the target template exists in the programs working
+    directory
+    '''
+    import argparse
+    parser = argparse.ArgumentParser(description='Test 1')
+    parser.add_argument("filename", help='Example: Joe schmoe -> jschmoe',
+            type=str)
+    return parser.parse_args()
+
+
+
+if __name__ == '__main__':
+    args = args()
+    result = loadFile(args.filename)
+    if result == True:
+        print("Test passed")
+    else:
+        print("Test Failed")
