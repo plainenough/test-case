e8174a6e9eb50f3e257c1393cb1efc46a30ee373
Derrick Walton <derrick.e.walton@gmail.com>
2019-07-26 01:03:47 -0600

    finalize level3

---
+++ level3/test_level3.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import pytest
+
+
+@pytest.mark.parametrize(
+        "filename",
+        ['dwalton',
+         'kgibson',
+         'adelacruz',
+         'ohuweih'])
+def test_original(filename):
+    myfile = __import__(filename)
+    (original, upper, lower, endup, begup)  = myfile.main()
+    assert original == 'Hello world', "Test failed"
+
+
+@pytest.mark.parametrize(
+        "filename",
+        ['dwalton',
+         'kgibson',
+         'adelacruz',
+         'ohuweih'])
+def test_upper(filename):
+    myfile = __import__(filename)
+    (original, upper, lower, endup, begup)  = myfile.main()
+    assert upper == 'HELLO WORLD', "Test failed"
+
+
+@pytest.mark.parametrize(
+        "filename",
+        ['dwalton',
+         'kgibson',
+         'adelacruz',
+         'ohuweih'])
+def test_lower(filename):
+    myfile = __import__(filename)
+    (original, upper, lower, endup, begup)  = myfile.main()
+    assert lower == 'hello world', "Test failed"
+
+
+@pytest.mark.parametrize(
+        "filename",
+        ['dwalton',
+         'kgibson',
+         'adelacruz',
+         'ohuweih'])
+def test_endup(filename):
+    myfile = __import__(filename)
+    (original, upper, lower, endup, begup)  = myfile.main()
+    assert endup == 'hellO worlD', "Test failed"
+
+
+@pytest.mark.parametrize(
+        "filename",
+        ['dwalton',
+         'kgibson',
+         'adelacruz',
+         'ohuweih'])
+def test_begup(filename):
+    myfile = __import__(filename)
+    (original, upper, lower, endup, begup)  = myfile.main()
+    assert begup == 'Hello World', "Test failed"
