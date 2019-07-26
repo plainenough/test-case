fa3a6aa4e087dae54d4b53c02c62cee49ae25bcb
Derrick Walton <dwalton@jane.com>
2019-07-26 12:12:22 -0600

    change test file to include dylan

--- level3/test_level3.py
+++ level3/test_level3.py
@@ -1,61 +1,39 @@
 #!/usr/bin/env python3
 import pytest
 
+userlist = ['dwalton', 'kgibson', 'adelacruz',
+            'dpfister', 'ohuweih']
 
-@pytest.mark.parametrize(
-        "filename",
-        ['dwalton',
-         'kgibson',
-         'adelacruz',
-         'ohuweih'])
+
+@pytest.mark.parametrize("filename", userlist)
 def test_original(filename):
     myfile = __import__(filename)
     (original, upper, lower, endup, begup) = myfile.main()
     assert original == 'Hello world', "Test failed"
 
 
-@pytest.mark.parametrize(
-        "filename",
-        ['dwalton',
-         'kgibson',
-         'adelacruz',
-         'ohuweih'])
+@pytest.mark.parametrize("filename", userlist)
 def test_upper(filename):
     myfile = __import__(filename)
     (original, upper, lower, endup, begup) = myfile.main()
     assert upper == 'HELLO WORLD', "Test failed"
 
 
-@pytest.mark.parametrize(
-        "filename",
-        ['dwalton',
-         'kgibson',
-         'adelacruz',
-         'ohuweih'])
+@pytest.mark.parametrize("filename", userlist)
 def test_lower(filename):
     myfile = __import__(filename)
     (original, upper, lower, endup, begup) = myfile.main()
     assert lower == 'hello world', "Test failed"
 
 
-@pytest.mark.parametrize(
-        "filename",
-        ['dwalton',
-         'kgibson',
-         'adelacruz',
-         'ohuweih'])
+@pytest.mark.parametrize("filename", userlist)
 def test_endup(filename):
     myfile = __import__(filename)
     (original, upper, lower, endup, begup) = myfile.main()
     assert endup == 'hellO worlD', "Test failed"
 
 
-@pytest.mark.parametrize(
-        "filename",
-        ['dwalton',
-         'kgibson',
-         'adelacruz',
-         'ohuweih'])
+@pytest.mark.parametrize("filename", userlist)
 def test_begup(filename):
     myfile = __import__(filename)
     (original, upper, lower, endup, begup) = myfile.main()
